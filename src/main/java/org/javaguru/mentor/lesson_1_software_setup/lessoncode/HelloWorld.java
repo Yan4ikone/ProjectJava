77e3bd31ffe08b2f80ebaed4611b5b895a879382
Yan <yantheone1@gmail.com>
2025-01-22 08:50:11 +0300

    new_repository

--- src/main/java/org/javaguru/mentor/lesson_1_software_setup/lessoncode/HelloWorld.java
+++ src/main/java/org/javaguru/mentor/lesson_1_software_setup/lessoncode/HelloWorld.java
@@ -3,8 +3,9 @@ package org.javaguru.mentor.lesson_1_software_setup.lessoncode;
 class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Valera");
+        System.out.println("Vasya");
         System.out.println("Kovs");
+
     }
 
 }
