535e5bb4bbd51a97a9265784a46d4094e00fc4d1
Yan <yantheone1@gmail.com>
2025-01-27 12:39:37 +0300

    new_repository

--- src/main/java/student_yan/lesson_3/PlaneTicketApp.java
+++
@@ -1,9 +0,0 @@
-package student_yan.lesson_3;
-
-public class PlaneTicketApp {
-    public static void main(String[] args) {
-
-        PlaneTicket ticket1 = new PlaneTicket("bishkek", "moscow", 11, 18);
-
-    }
-}
--- src/main/java/student_yan/lesson_3/Robot.java
+++
@@ -1,9 +0,0 @@
-package student_yan.lesson_3;
-
-public class Robot {
-
-    public static void main(String[] args) {
-
-
-    }
-}
